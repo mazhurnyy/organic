167f355ea6e1e242c3f0dd51f957b269617f2d14
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-02-02 08:37:34 +0200

    brands

--- js/brands.js
+++ js/brands.js
@@ -1,17 +1,26 @@
 $(function () {
     $(document)
+        .ready(function () {
+            if ($("#wrapper").outerWidth() < 768) {
+                $("#content-brands .details-categories").removeAttr("open");
+            } else {
+                $("#content-brands .details-categories").attr("open", "open");
+            }
+        })
         .on("click", ".brands-filter li.button", function () {
             $(this).closest(".brands-filter").find("li.active").removeClass("active").addClass("button");
             $(this).removeClass("button").addClass("active");
 
             if (this.hasAttribute("data-c-clean")) {
                 $(".brand-link").removeClass("d-none");
+
             } else if (this.hasAttribute("data-c")) {
                 $(".brand-link").addClass("d-none");
                 $(".brand-link.c-" + $(this).data("c")).removeClass("d-none");
 
             } else if (this.hasAttribute("data-l-clean")) {
                 $(".brand-group").removeClass("d-none");
+
             } else if (this.hasAttribute("data-l")) {
                 $(".brand-group").addClass("d-none");
                 $(".brand-group.l-" + $(this).data("l")).removeClass("d-none");
