18ea71c4996f55da06733fef391287dae5b999a8
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-05-11 23:17:48 +0300

    navbar 3 lvl

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,9 @@
 $(function () {
+    $(document).on("click", ".navbar-toggle", function () {
+        $(".navbar").toggleClass("open");
+        $(".navbar-toggle").toggleClass("open");
+    });
+
     $(document)
         .on("click", "#wrapper", function (e) {
             if (mobile && e.target.closest(".navbar-lvl-1") === null) {
@@ -31,9 +36,39 @@ $(function () {
 
             $(".navbar-lvl-1").removeClass("open");
         })
-        .on("click", ".navbar-toggle", function () {
-            $(".navbar").toggleClass("open");
-            $(".navbar-toggle").toggleClass("open");
+    ;
+
+    $(document)
+        .on("click", "#wrapper", function (e) {
+            if (mobile && e.target.closest(".navbar-lvl-2") === null) {
+                $(".navbar-lvl-2").removeClass("open");
+            }
+        })
+        .on("click", ".navbar-lvl-2 > a", function (e) {
+            if ($("#wrapper").outerWidth() < 768 || mobile) {
+                if ($(this).next().length > 0) {
+                    e.preventDefault();
+                }
+
+                const prev = $(this).parent().hasClass("open");
+
+                $(".navbar-lvl-2").removeClass("open");
+
+                if (!prev) {
+                    $(this).parent().addClass("open");
+                }
+            }
+        })
+        .on("mouseover", ".navbar-lvl-2", function () {
+            if ($("#wrapper").outerWidth() < 768 || mobile) return false;
+
+            $(".navbar-lvl-2").removeClass("open");
+            $(this).addClass("open");
+        })
+        .on("mouseleave", ".navbar-lvl-2", function () {
+            if ($("#wrapper").outerWidth() < 768 || mobile) return false;
+
+            $(".navbar-lvl-2").removeClass("open");
         })
     ;
 });
